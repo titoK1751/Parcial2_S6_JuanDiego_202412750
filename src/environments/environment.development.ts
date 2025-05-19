d327f00865e4585d43f75983889f9acdc1878357
Juan Diego <juandiqa@hotmail.com>
2025-05-19 15:44:49 -0500

    Configuracion environment y bootrstrap

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+const baseUrl = 
+"https://raw.githubusercontent.com/NoNameLab/ISIS2603_Parcial2_S6_202510/refs/heads/main/flights/";
+
+export const environment = {
+ production: false,
+ baseUrl
+};
